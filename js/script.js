d0c493e208be5de8dd461f67479d25e7be877cb2
Sovranoh <143640795+Sovranoh@users.noreply.github.com>
2026-07-29 16:27:37 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+// ===============================
+// Noir Coffee - Cart System
+// ===============================
 
+// قراءة السلة إذا موجودة
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// حفظ السلة
+function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// إضافة منتج
+function addToCart(id, name, price) {
+
+    // هل المنتج موجود؟
+    const existingItem = cart.find(item => item.id === id);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({
+            id: id,
+            name: name,
+            price: price,
+            quantity: 1
+        });
+    }
+
+    saveCart();
+
+    alert(name + " added to cart");
+}
+
+// عرض محتوى السلة (للتجربة)
+function showCart() {
+    console.log(cart);
+}
